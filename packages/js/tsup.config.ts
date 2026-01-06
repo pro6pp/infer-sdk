f302f2bd864e4705ff661c20c7a7209008386f61
eduard <eduard@d-centralize.nl>
2026-01-06 12:36:03 +0100

    fix: revert tsup file name

--- packages/js/tsup.config.ts
+++ packages/js/tsup.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: {
-    'pro6pp-infer': 'src/index.ts',
-  },
+  entry: ['src/index.ts'],
   format: ['cjs', 'esm', 'iife'],
   globalName: 'Pro6PP',
   dts: true,
