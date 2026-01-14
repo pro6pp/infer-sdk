82e192cd28dfa8f0ad8413b5c3b6d76144765d0a
eduard <eduard@d-centralize.nl>
2026-01-14 16:38:25 +0100

    chore: revert changes to tsup config and delete test files

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -2,20 +2,9 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.tsx'],
-  format: ['cjs', 'esm', 'iife'],
-  globalName: 'Pro6PPReact',
+  format: ['cjs', 'esm'],
   dts: true,
   clean: true,
-  external: ['react', 'react-dom'],
+  external: ['react'],
   noExternal: ['@pro6pp/infer-core'],
-  target: 'es2017',
-  // FIX: Replace process.env.NODE_ENV with a string for the browser
-  define: {
-    'process.env.NODE_ENV': '"development"',
-  },
-  outExtension({ format }) {
-    return {
-      js: format === 'iife' ? '.global.js' : format === 'cjs' ? '.cjs' : '.mjs',
-    };
-  },
 });
